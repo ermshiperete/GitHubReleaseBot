2ef670d7e3833114ac2858d3414f5f0b0ecc23a0
Eberhard Beilharz <eb1@sil.org>
2019-10-30 11:30:44 +0700

    Initial commit

---
+++ GitHubReleaseBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHubReleaseBot
+{
+	public static class Program
+	{
+		private static void Main(string[] args)
+		{
+			TcpHelper.StartServer(80);
+			TcpHelper.Listen();
+		}
+	}
+}
